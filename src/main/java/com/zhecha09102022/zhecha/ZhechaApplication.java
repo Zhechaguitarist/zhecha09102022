b95769d1efa33610748cccb008ce1f385a56b797
puhov <puhov@bifit.com>
2022-10-09 02:40:57 +0300

    zhecha09102022

---
+++ src/main/java/com/zhecha09102022/zhecha/ZhechaApplication.java
@@ -0,0 +1,13 @@
+package com.zhecha09102022.zhecha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhechaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhechaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhecha09102022/zhecha/ZhechaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhecha09102022.zhecha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhechaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
