0e79e9eed656825f16b4928e743c62a2f46b243f
puhov <puhov@bifit.com>
2022-10-09 02:53:24 +0300

    Add HelloController

---
+++ src/main/java/com/zhecha09102022/zhecha/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.zhecha09102022.zhecha.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello!";
+    }
+}
